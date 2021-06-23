9cae1b86c7d05c116fcdaff9f402ccc8b7a8832f
Admin <Admin@DESKTOP-6LB9OOE>
2021-06-23 14:35:06 +0900

    첫번째

---
+++ firstProject/src/firstProject/AppMain.java
@@ -0,0 +1,13 @@
+package firstProject;
+
+public class AppMain {
+//	클래스명은 항상 대문자로 작성.	
+	// 주석문 : 프로그램실행과는 관계없이 실행.
+	public static void main(String[] args) {
+//		 주석문 : 프로그램실행과는 관계없이 실행.
+		double age = 30.4;
+		String name = "강민정";
+		System.out.println("내 이름은 : " + name);
+		System.out.println("내 나이는 : " + age);
+	}
+}
